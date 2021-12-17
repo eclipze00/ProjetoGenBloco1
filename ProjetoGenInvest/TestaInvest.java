a2bd3edadf34ce819032df501c9feb8af196c0a6
Guilherme Barbosa Rogrigues <gui_tsuna@hotmail.comgit config --global user.email gui_tsuna@hotmail.comgit config --global user.email gui_tsuna@hotmail.com>
2021-12-16 22:14:42 -0300

    Projeto Gen I

--- ProjetoGenInvest/TestaInvest.java
+++ ProjetoGenInvest/TestaInvest.java
@@ -19,9 +19,10 @@ public class TestaInvest extends Investimento {
 		f1.setInvestMin(100.00);
 		f1.setPrecoUni(114.16);
 		f1.setVencF("12/12/2022");
-		f1.setSobre("A principal característica dos fundos da classe “Renda Fixa” que recebem o sufixo “Curto Prazo” está relacionada ao vencimento dos títulos que compõem a sua carteira," +
-			    "que precisam respeitar o prazo máximo a decorrer de 375 dias, e prazo médio da carteira inferior a 60 dias" + 
-			   "ATENÇÃO: "+ "Os fundos da classe “Renda Fixa” que recebem os sufixos “Curto Prazo”, “Referenciado” e “Simples” podem utilizar derivativos apenas para proteção da carteira.");
+		f1.setSobre(  "A principal característica dos fundos da classe “Renda Fixa” que recebem o sufixo “Curto Prazo” está relacionada ao vencimento dos títulos que compõem a sua carteira," 
+					+ "\nque precisam respeitar o prazo máximo a decorrer de 375 dias, e prazo médio da carteira inferior a 60 dias" 
+			    	+ "\nATENÇÃO: "
+			    	+ "Os fundos da classe “Renda Fixa” que recebem os sufixos “Curto Prazo”, “Referenciado” e “Simples” podem utilizar derivativos apenas para proteção da carteira.");
 		
 		System.out.println("Sobre: " + f1.getSobre());
 		System.out.println();
@@ -31,7 +32,7 @@ public class TestaInvest extends Investimento {
 		System.out.println("Preço Unitário: " + "R$" + f1.getPrecoUni());
 		System.out.println("Vencimento: " + f1.getVencF());
 		System.out.println();
-		System.out.println("================================================================================================================================================================");
+		System.out.println("====================================================================================================================================================================================");
 				
 		//Tesouro Direto
 		System.out.println();
@@ -42,7 +43,7 @@ public class TestaInvest extends Investimento {
 		t1.setVencF("01/07/2024");
 		t1.setSobre("Indicado para aqueles que querem realizar investimentos de curto prazo."
 				+ "\nTítulo prefixado, ou seja, no momento da compra, você já sabe exatamente quanto irá receber no futuro "
-				+ "(sempre R$ 1.000 por unidade de título) " + "\nÉ mais interessante para quem pode deixar o seu dinheiro render até o vencimento do investimento, pois não paga juro semestrais."
+				+ "\n(sempre R$ 1.000 por unidade de título) " + "\nÉ mais interessante para quem pode deixar o seu dinheiro render até o vencimento do investimento, pois não paga juro semestrais."
 				+ "\nEm caso de resgate antecipado, o Tesouro Nacional garante sua recompra pelos seus valores de mercado.");
 				
 		System.out.println("Sobre: " + t1.getSobre());
@@ -53,25 +54,25 @@ public class TestaInvest extends Investimento {
 		System.out.println("Preço Unitário: " + "R$" + t1.getPrecoUni());
 		System.out.println("Vencimento: " + t1.getVencF());
 		System.out.println();
-		System.out.println("================================================================================================================================================================");
+		System.out.println("====================================================================================================================================================================================");
 
 		//Variavel
 		System.out.println();
 		v1.setTitulo("Renda Variavel"); 
 		v1.setRentA(12.50); // Porcetagem 
-		v1.setInvestMin(250.00;); // Valor minimo
+		v1.setInvestMin(250.00); // Valor minimo
 		v1.setPrecoUni(1000.00); // valor maximo --> ?
-		v1.setVencF("07/12/2023"); //tempo de investimento 
+		v1.setVencF("Sem tempo definido."); //tempo de investimento 
 		v1.setSobre("A renda variável consiste em diferentes tipos de ativos financeiros com uma mesma característica: rentabilidade imprevisível."
-			    + "Por isso, seus investimentos costumam estar associados à palavra volatilidade." 
-			    + "Normalmente, essa instabilidade acontece por esses ativos dependerem muito do cenário econômico e político do país, assim como do desempenho de empresas e áreas do mercado privado."
-			    + "Ou seja, em uma hora seu investimento pode estar subindo e minutos depois caindo próximo a zero.");
+			    + "\nPor isso, seus investimentos costumam estar associados à palavra volatilidade." 
+			    + "\nNormalmente, essa instabilidade acontece por esses ativos dependerem muito do cenário econômico e político do país, assim como do desempenho de empresas e áreas do mercado privado."
+			    + "\nOu seja, em uma hora seu investimento pode estar subindo e minutos depois caindo próximo a zero.");
 			    /*
 			    Criptomoedas
-Bitcoin, Tether, Dash, Ripple… os nomes das criptomoedas ficaram bem famosos nos últimos anos. E elas também são consideradas um tipo de renda variável.
-Aqui no blog da CashMe já falamos mais a fundo sobre as criptomoedas, mas, de forma resumida, podemos entendê-las como moedas virtuais usadas como forma de pagamento,
-sem intermédio de bancos.
-Elas são um exemplo bem claro de como a renda variável oscila bastante. Basta avaliar a valorização e desvalorização da bitcoin.*/
+				Bitcoin, Tether, Dash, Ripple… os nomes das criptomoedas ficaram bem famosos nos últimos anos. E elas também são consideradas um tipo de renda variável.
+				Aqui no blog da CashMe já falamos mais a fundo sobre as criptomoedas, mas, de forma resumida, podemos entendê-las como moedas virtuais usadas como forma de pagamento,
+				sem intermédio de bancos.
+				Elas são um exemplo bem claro de como a renda variável oscila bastante. Basta avaliar a valorização e desvalorização da bitcoin.*/
 				
 		System.out.println("Sobre: " + v1.getSobre());
 		System.out.println();
